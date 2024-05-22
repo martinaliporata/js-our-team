7f5e7d0d92501dc2296d64c35eb75e8c35ce87f9
martinaliporata <martinaliporata@gmail.com>
2024-05-22 14:54:02 +0200

    aggiunto array di oggetti

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const workers = 
+    [
+        {
+            name : 'Wayne Barnett',
+            'job-title' : 'Founder & CEO',
+            img : 'wayne-barnett-founder-ceo.jpg'
+        },
+        {
+            name : 'Angela Caroll',
+            'job-title' : 'Chief Editor',
+            img : 'angela-caroll-chief-editor.jpg'
+        },
+        {
+            name : 'Walter Gordon',
+            'job-title' : 'Office Manager',
+            img : 'walter-gordon-office-manager.jpg'
+        },
+        {
+            name : 'Angela Lopez',
+            'job-title' : 'Social Media Manager',
+            img : 'angela-lopez-social-media-manager.jpg'
+        },
+        {
+            name : 'Scott Estrada',
+            'job-title' : 'Developer',
+            img : 'scott-estrada-developer.jpg'
+        },
+        {
+            name : 'Barbara Ramos',
+            'job-title' : 'Graphic Designer',
+            img : 'barbara-ramos-graphic-designer.jpg'
+        }
+
+    ]
